02997979dee6049842b6575feaa7c008480c0037
JM <s9923024@gmail.com>
2021-07-23 17:37:40 +0800

    Update 2021-07-23T09:37:35.944Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,9 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  $('.ham-icon').click(function (e) {
+    e.preventDefault();
+    $('.menu').toggleClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
